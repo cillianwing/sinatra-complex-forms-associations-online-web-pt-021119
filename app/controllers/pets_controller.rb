70700f8e5f64cbc28549f4a4d68019a924840450
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-06 17:36:38 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,6 +27,7 @@ class PetsController < ApplicationController
   patch '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
+    if 
     redirect to "pets/#{@pet.id}"
   end
 end
