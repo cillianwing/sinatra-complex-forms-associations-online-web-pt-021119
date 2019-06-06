08b95ee45b23db332c3747effab0f129a2b2040d
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-06 17:36:13 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,7 +25,7 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    @pet = Pet.find(params[:])
+    @pet = Pet.find(params[:id])
     redirect to "pets/#{@pet.id}"
   end
 end
